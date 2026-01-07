0c572bde29011c7378269b1b789ab0a9d312e3be
Mohammed Shiraz <lightningboy78@gmail.com>
2026-01-07 11:14:17 -0600

    deploying to find the keys 10

--- amplify/functions/admin-actions/resource.ts
+++ amplify/functions/admin-actions/resource.ts
@@ -1,7 +1,7 @@
 import { defineFunction } from "@aws-amplify/backend";
 
 export const adminActions = defineFunction({
-    name: "admin-actions",
+    name: "adminActions",
     entry: "./handler.ts",
     timeoutSeconds: 30, // Cognito admin actions can be slow
 });
